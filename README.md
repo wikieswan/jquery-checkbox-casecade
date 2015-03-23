3ba9bb367db2e9aa1eea4d271a28c701d3d36a0b
wikies.wan <wikies.wan@vipshop.com>
2015-03-23 19:56:58 +0800

    fix

--- README.md
+++ README.md
@@ -10,27 +10,36 @@ will do all this for you .
 
 ## how to use it 
 
-###1 include jquery-checkbox-casecade.js in your html file . Please make sure you have included jquery before this.
+###1 include jquery-checkbox-casecade.js in your html file . 
+
+Please make sure you have included jquery before this.
 
 	<script type="text/javascript" src="jquery.min.js"></script>
 	<script type="text/javascript" src="jquery-checkbox-casecade.js"></script>
 
-###2 add an spacial attribute ```data-chk``` in html input:checkbox tag 
+###2 add an spacial attribute ```data-id``` ```data-parent``` in html input:checkbox tag 
 
 	<div id="checkboxContent1">
 		<label>
-			<input type="checkbox" name="name1" data-chk="1"/>all
-		</label>
+			<input type="checkbox" name="name1" data-id="p0" data-parent="-1"/>all-0</label>
 		<label>
-			<input type="checkbox" name="name1" data-chk="0"/>item1
-		</label>
+			<input type="checkbox" name="name1" data-id="p1" data-parent="0"/>reverse-select</label>
+		<br>
 		<label>
-			<input type="checkbox" name="name1" data-chk="0"/>item2
-		</label>
+			<input type="checkbox" name="name1" data-id="p10" data-parent="p0"/>p0-10</label>
+		<label>
+			<input type="checkbox" name="name1" data-id="p11" data-parent="p0"/>p0-11</label>
+		<br>
+		<label>
+			<input type="checkbox" name="name1" data-id="p20" data-parent="p10"/>p10-20</label>
+		<label>
+			<input type="checkbox" name="name1" data-id="p21" data-parent="p10"/>p10-21</label>
 	</div>
 
-```data-chk="1"``` means it is the "select all" guy. And ```data-chk="0"``` means it is the "select all"'s items
-The "select all" and its items have the same name value.
+Every input:checkbox has ```data-id``` and ```data-parent``` attr;```data-id``` means its own ID----only one;```data-parent``` means its parent's ID.Whit the two att, you can find every checkbox in the tree.
+
+```data-parent="-1"``` means it is the root selection.  ```data-parent="0"``` means it is reverse selecttion  
+
 
 ###3 do action ! 
 
--- jquery-checkbox-casecade.js
+++ jquery-checkbox-casecade.js
@@ -1,5 +1,6 @@
 /**
 casecade -- input checkbox casecade jquery plug-in
+wikies.wan
 */
 $.fn.casecade = function () {
 	//this is input:checkbox DOM content , one of their parents
@@ -8,29 +9,107 @@ $.fn.casecade = function () {
     //bind change event to input:checkbox's parent . when someone change,we checkout all the seleced input:checkbox and make sure the others act in a right way
     $chkContent.off('change.casecade').on('change.casecade',':checkbox',function(e){ 
     	var target = e.target ? e.target : e.srcElement;
-    	casecade(target);
+    	eventDo(target);
     });
 
-    function casecade(target){
-	    var name = $(target).attr('name'),
-		    chk =  $(target).data('chk'),
+    
+	function eventDo(target){
+		var name = $(target).attr('name'),
+		    id = $(target).data('id'),
+		    parent = $(target).data('parent'),
 		    checked = target.checked,
-		    $chks = $('input[name="'+name+'"][data-chk="0"]'),
-		    $theAllOne = $('input[name="'+name+'"][data-chk="1"]'),
+
+		    $chks = $('input[name="'+name+'"]'), //all checkbox		    
+		    $rootChk = $('input[name="'+name+'"][data-parent="-1"]'), // root checkbox
+		    $parentChk = $('input[name="'+name+'"][data-id="'+parent+'"]'), // target's parent checkbox
+		    $brotherChks = $('input[name="'+name+'"][data-parent="'+parent+'"]'), // target's brothers checkbox
+		    $childrenChks = $('input[name="'+name+'"][data-parent="'+id+'"]'), // target's brothers checkbox
+		    $offspring, //target's all offspring
 		    count = 0;
-		if(chk==1){
-			for(var i=0,len=$chks.length,e;i<len;i++){
+		if(parent===0){//reverse select
+			for(var i=0,len=$chks.length,e,e,eId,eChildren;i<len;i++){
 				e = $chks[i];
-				e.checked = checked;
+				eId = $(e).data('id');
+				$eChildren = $('input[name="'+name+'"][data-parent="'+eId+'"]');
+				if($eChildren.length===0){
+					$(e).click();
+				}
 			}
+			return ;
 		}
-		else{
-			for(var i=0,len=$chks.length,e;i<len;i++){
-				e = $chks[i];
+
+
+		if($childrenChks.length===0){// leaf node change event
+			if(parent===-1){
+				return ;
+			}
+			for(var i=0,len=$brotherChks.length,e;i<len;i++){ // for-each it's brothers ,make sure it's parent is checked or not
+				e = $brotherChks[i];
 				e.checked?count ++:'';
 			}
-
-			$theAllOne[0].checked = count==$chks.length;
+			
+			$parentChk[0].checked = count===$brotherChks.length;
+			casecadeDo(target);
 		}
+		else{//not leaf node change event
+			$offspring = getOffspring(target,[])
+			for(var i=0,len=$offspring.length,e;i<len;i++){
+				e = $offspring[i];
+				e.checked = checked;
+			}
+
+			casecadeDo(target);
+		}
+
+		
 	}
+	function casecadeDo(target){
+		var name = $(target).attr('name'),
+		    id = $(target).data('id'),
+		    parent = $(target).data('parent'),
+		    checked = target.checked,
+		    $parentChk = $('input[name="'+name+'"][data-id="'+parent+'"]'), // target's parent checkbox
+		    $brotherChks = $('input[name="'+name+'"][data-parent="'+parent+'"]'), // target's brothers checkbox
+		    count = 0;
+		
+		if(parent==-1){
+			return ;
+		}
+		else{
+			target = $parentChk[0];
+			casecadeDo(target);
+		}
+
+		for(var i=0,len=$brotherChks.length,e;i<len;i++){ // for-each it's brothers ,make sure it's parent is checked or not
+			e = $brotherChks[i];
+			e.checked?count ++:'';
+		}
+		
+		$parentChk[0].checked = count===$brotherChks.length;		
+		
+	}
+
+	function getOffspring(target,obj){
+		var name = $(target).attr('name'),
+		    id = $(target).data('id'),
+		    parent = $(target).data('parent'),
+		    checked = target.checked,
+		    $childrenChks = $('input[name="'+name+'"][data-parent="'+id+'"]'); 
+		
+		if($childrenChks.length===0){
+			return obj
+		}
+		else {
+			var result;
+			for(var i=0,len=$childrenChks.length,e;i<len;i++){
+				e = $childrenChks[i];
+				target = e;
+				obj.push(target)
+				result = getOffspring(target,obj)
+			}
+			return result;
+		}
+		
+	}
+
 };
